3723679a764620bbd4b4345aa2b688e1711ae807
naldal <cec7378@gmail.com>
2020-08-16 16:04:41 +0900

    5장 끝

--- hamin/src/App.js
+++ hamin/src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import EventPractice7 from './EventPractice7.js';
+import React, { Component } from 'react';
+import ValidationSample from './ValidationSample.js';
+import ScrollBox from './ScrollBox.js';
 
-const App = () => {
-    return (
-        <div>
-            <EventPractice7 />
-        </div>
-    );
-};
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+                <button onClick={() => this.scrollBox.scrollToBottom()}>
+                    맨 밑으로
+                </button>
+            </div>
+        );
+    }
+}
 
 export default App;
