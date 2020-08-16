04227d23ea75d93e34acf706f8e45d217a1fa117
naldal <cec7378@gmail.com>
2020-08-16 16:53:31 +0900

    6장 끝

--- hamin/src/App.js
+++ hamin/src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import ValidationSample from './ValidationSample.js';
-import ScrollBox from './ScrollBox.js';
+import IterationSample from './IterationSample.js';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-                <button onClick={() => this.scrollBox.scrollToBottom()}>
-                    맨 밑으로
-                </button>
+                <IterationSample />
             </div>
         );
     }
