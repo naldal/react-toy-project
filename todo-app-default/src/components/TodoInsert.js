221b1b691a54bd2131e15630a6fcb473ed7a2752
naldal <cec7378@gmail.com>
2020-08-24 00:19:56 +0900

    created pj to blog post

--- todo-app-default/src/components/TodoInsert.js
+++ todo-app-default/src/components/TodoInsert.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
 // 새로운 항목을 입력하고 추가할 수 있는 컴포넌트. state를 통해 인풋의 상태를 관리
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback((e) => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        (e) => {
+            onInsert(value);
+            setValue('');
+
+            e.preventDefault();
+        },
+        [onInsert, value]
+    );
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="할일을 입력하세요"></input>
-            <button type="submit">
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input placeholder="할일을 입력하세요" value={value} onChange={onChange} />
+            <button type="submit" onSubmit={onSubmit}>
                 <MdAdd />
             </button>
         </form>
--- todo-app-default/src/components/TodoListItem.js
+++ todo-app-default/src/components/TodoListItem.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from 'react-icons/md';
+import cn from 'classnames';
 import './TodoListItem.scss';
 
 // 각 할 일 항목에 대한 정보를 보여주는 컴포넌트, todo 객체를 props로 받아와서 상태에 따라 다른 스타일의 UI를 보여줌
-const TodoListItem = () => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+    const { id, text, checked } = todo;
+
     return (
         <div className="TodoListItem">
-            <div className="checkbox">
-                <MdCheckBoxOutlineBlank />
-                <div className="text">할 일</div>
+            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                <div className="text">{text}</div>
             </div>
-            <div className="remove">
+            <div className="remove" onClick={() => onRemove(id)}>
                 <MdRemoveCircleOutline />
             </div>
         </div>
