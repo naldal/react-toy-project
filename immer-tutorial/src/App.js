a49e346346d13308fa17870a99da517a11d71cb2
naldal <cec7378@gmail.com>
2020-08-25 13:52:48 +0900

    12장 끝

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
 
 const App = () => {
     const nextId = useRef(1);
@@ -6,16 +7,18 @@ const App = () => {
     const [data, setData] = useState({ array: [], uselessValue: null });
 
     //input 수정을 위한 함수
-    const onChange = useCallback(
-        (e) => {
-            const { name, value } = e.target;
-            setForm({
-                ...form,
-                [name]: [value],
-            });
-        },
-        [form]
-    );
+    const onChange = useCallback((e) => {
+        const { name, value } = e.target;
+        // setForm({
+        //     ...form,
+        //     [name]: [value],
+        // });
+        setForm(
+            produce((draft) => {
+                draft[name] = value;
+            })
+        );
+    }, []);
 
     //form 등록을 위한 함수
     const onSubmit = useCallback(
@@ -28,10 +31,15 @@ const App = () => {
             };
 
             //array에 새 항목 등록
-            setData({
-                ...data,
-                array: data.array.concat(info),
-            });
+            // setData({
+            //     ...data,
+            //     array: data.array.concat(info),
+            // });
+            setData(
+                produce((draft) => {
+                    draft.array.push(info);
+                })
+            );
 
             //form 초기화
             setForm({
@@ -39,19 +47,24 @@ const App = () => {
                 username: '',
             });
         },
-        [data, form.name, form.username]
+        [form.name, form.username]
     );
 
     //항목을 삭제하는 함수
-    const onRemove = useCallback(
-        (id) => {
-            setData({
-                ...data,
-                array: data.array.filter((info) => info.id !== id),
-            });
-        },
-        [data]
-    );
+    const onRemove = useCallback((id) => {
+        // setData({
+        //     ...data,
+        //     array: data.array.filter((info) => info.id !== id),
+        // });
+        setData(
+            produce((draft) => {
+                draft.array.splice(
+                    draft.array.findIndex((info) => info.id === id),
+                    1
+                );
+            })
+        );
+    }, []);
 
     return (
         <div>
