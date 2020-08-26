fd01285ec6580248fcdbd74b1fa189d0837643a5
naldal <cec7378@gmail.com>
2020-08-26 21:01:27 +0900

    p381

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,33 +1,17 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useCallback } from "react";
+
+import NewsList from "./NewsList";
+import Categories from "./Categories";
 
 const App = () => {
-   const [data, setData] = useState(null);
-   const onClick = async () => {
-      try {
-         const response = await axios.get(
-            'https://jsonplaceholder.typicode.com/todos/1',
-         );
-         setData(response.data);
-      } catch (e) {
-         console.log(e);
-      }
-   };
-
-   return (
-      <div>
-         <div>
-            <button onClick={onClick}>불러오기</button>
-         </div>
-         {data && (
-            <textarea
-               rows={7}
-               value={JSON.stringify(data, null, 2)}
-               readOnly={true}
-            />
-         )}
-      </div>
-   );
+    const [category, setCategory] = useState("all");
+    const onSelect = useCallback((category) => setCategory(category), []);
+    return (
+        <>
+            <Categories category={category} onSelect={onSelect} />
+            <NewsList category={category} />
+        </>
+    );
 };
 
 export default App;
