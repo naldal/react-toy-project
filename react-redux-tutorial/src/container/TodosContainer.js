c829daa96e09b26946a658cc91a8fd959131e5b3
naldal <cec7378@gmail.com>
2020-09-01 09:00:53 +0900

    17장 끝

--- react-redux-tutorial/src/container/TodosContainer.js
+++ react-redux-tutorial/src/container/TodosContainer.js
@@ -27,38 +27,3 @@ const TodosContainer = () => {
 };
 
 export default React.memo(TodosContainer);
-
-// const TodosContainer = ({
-//   input,
-//   todos,
-//   changeInput,
-//   insert,
-//   toggle,
-//   remove,
-// }) => {
-//   return (
-//     <Todos
-//       input={input}
-//       todos={todos}
-//       onChangeInput={changeInput}
-//       onInsert={insert}
-//       onToggle={toggle}
-//       onRemove={remove}
-//     ></Todos>
-//   );
-// };
-
-// export default connect(
-//   //비구조화 할당을 통해 todos를 분리하여
-//   //state.todos.input 대신 todos.input을 사용
-//   ({ todos }) => ({
-//     input: todos.input,
-//     todos: todos.todos,
-//   }),
-//   {
-//     changeInput,
-//     insert,
-//     toggle,
-//     remove,
-//   },
-// )(TodosContainer);
