3282a437a24f366edcb7f4e814cd7b74cc01bb0e
naldal <cec7378@gmail.com>
2020-09-02 13:17:59 +0900

    created toy project - movie searcher

---
+++ moviedb/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Search from './components/Search';
+import axios from 'axios';
+import Results from './components/Results';
+
+function App() {
+  const [state, setState] = useState({
+    s: '',
+    results: [],
+    selected: {},
+  });
+  const apiurl = 'http://www.omdbapi.com/?i=tt3896198&apikey=1e14b21f';
+
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      axios(apiurl + '&s=' + state.s).then(({ data }) => {
+        let results = data.Search;
+
+        setState((prevState) => {
+          return { ...prevState, results: results };
+        });
+      });
+    }
+  };
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState((prevState) => {
+      return { ...prevState, s: s };
+    });
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Movie Database</h1>
+      </header>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+        <Results results={state.results} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
